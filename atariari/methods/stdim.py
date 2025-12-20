5263b5d8c62ea189f0c1aecc29c709e5027464ff
Giacomo Carfì <giacomo.carfi@outlook.it>
2025-12-20 09:27:13 +0100

    fix: encoder to device

--- atariari/methods/stdim.py
+++ atariari/methods/stdim.py
@@ -26,6 +26,7 @@ class Classifier(nn.Module):
 class InfoNCESpatioTemporalTrainer(Trainer):
     def __init__(self, encoder, config, device=torch.device('cpu'), wandb=None):
         super().__init__(encoder, wandb, device)
+        self.encoder = self.encoder.to(device)
         self.config = config
         self.patience = self.config["patience"]
         self.classifier1 = nn.Linear(self.encoder.hidden_size, self.encoder.local_layer_depth).to(device)  # x1 = global, x2=patch, n_channels = 32
